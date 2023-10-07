59ed9cfcec4d950a094e568856f10b99d37b3a69
NandoRodriguess7 <fernando.rodrigues.alves14@gmail.com>
2023-10-07 17:24:15 -0300

    Special move - En Passant

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -83,8 +83,8 @@ public class King extends ChessPiece {
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
-		//specialmove castling
+
+		// specialmove castling
 		if (getMoveCount() == 0 && !chessMatch.getCheck()) {
 			// #specialmove castling kingside rook
 			Position posT1 = new Position(position.getRow(), position.getColumn() + 3);
@@ -96,20 +96,19 @@ public class King extends ChessPiece {
 				}
 			}
 		}
-		
-		// #specialmove castling queenside rook
-					Position posT2 = new Position(position.getRow(), position.getColumn() - 4);
-					if (testRookCastling(posT2)) {
-						Position p1 = new Position(position.getRow(), position.getColumn() - 1);
-						Position p2 = new Position(position.getRow(), position.getColumn() - 2);
-						Position p3 = new Position(position.getRow(), position.getColumn() - 3);
-						if (getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
-							mat[position.getRow()][position.getColumn() - 2] = true;
-						}
-					}
-				}
 
-	return mat;
-}
+		// #specialmove castling queenside rook
+		Position posT2 = new Position(position.getRow(), position.getColumn() - 4);
+		if (testRookCastling(posT2)) {
+			Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+			Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+			Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+			if (getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+				mat[position.getRow()][position.getColumn() - 2] = true;
+			}
+		}
+
+		return mat;
+	}
 
 }
